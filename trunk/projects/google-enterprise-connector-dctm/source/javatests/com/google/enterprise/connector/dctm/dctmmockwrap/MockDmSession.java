a1d07d30f05998ca972303fe8e22917267829d2e
padrethedrey <padrethedrey@a5724643-9123-0410-9cf5-31ddb559c8c8>
2007-01-23 19:07:08 +0000

    Re-integrating a change that Jeremy made while I was doing that big check-in.  Jeremy, please check that this file is now ok.

--- trunk/projects/google-enterprise-connector-dctm/source/javatests/com/google/enterprise/connector/dctm/dctmmockwrap/MockDmSession.java
+++ trunk/projects/google-enterprise-connector-dctm/source/javatests/com/google/enterprise/connector/dctm/dctmmockwrap/MockDmSession.java
@@ -33,9 +33,7 @@ public class MockDmSession implements ISession {
 
 	public String getLoginTicketForUser(String username) {
 		//this assumes that Mock authenticated the session by
-		//checking username==paswword (which was the case at least till
-		//2007/01/19 but it is so aukward that I thought it could
-		//be changed anytime...)
+		//checking username==paswword 
 		return mockJcrSession.getUserID();//The only security here is inherent to the fact that if authentication failed, Session==null the returning getUserId instead of directly retuning username would throw a nullPointerException
 	}
 
@@ -56,9 +54,4 @@ public class MockDmSession implements ISession {
 		return dctmMockRepositoryDocument;
 	}
 
-
-	
-
-	
-	
 }
