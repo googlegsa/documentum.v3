9dd7af1d8fae76559007d8d3b6bd81a43290a238
Sylvain Cauchy <scauchyster@gmail.com>
2008-10-07 14:40:34 +0000

    Documentum Connector changes linked to Issue 32.
    Analysis:
    
    --------
    When the timestamp field of the dm_audittrail table has the format
    mm/dd/yyyy hh:mm:ss, in the query which retrieves documents to delete the
    timestamp is formatted as yyyy-dd-mm ... instead of yyyy-mm-dd.
    
    This problem could cause an infinite loop or make the connector never
    return any document to delete.
    
    
    Change Log:
    ----------
    
    *  projects/dctm-client/source/java/com/google/enterprise/connector/dctm/dctmdfcwrap/DmTime.java
      - Added asString() method returns a String
      - Added time_pattern44 attribute represents the IDfTime.DF_TIME_PATTERN44 String pattern "mm/dd/yyyy hh:mi:ss" 
      - Added getTime_pattern44() getter returns the time_pattern44 attribute 
      
    
    
    *  projects/dctm-client/source/java/com/google/enterprise/connector/dctm/dctmdfcwrap/DmCollection.java
      - Added getTime() method returns an ITime object

---
+++ tags/release_1.1.0_r446/dctm-client/source/java/com/google/enterprise/connector/dctm/dctmdfcwrap/DmQuery.java
@@ -0,0 +1,66 @@
+package com.google.enterprise.connector.dctm.dctmdfcwrap;
+
+import java.util.logging.Logger;
+
+import com.documentum.fc.client.DfQuery;
+import com.documentum.fc.client.DfSessionLeakException;
+import com.documentum.fc.client.IDfCollection;
+import com.documentum.fc.client.IDfSession;
+import com.documentum.fc.client.IDfQuery;
+import com.documentum.fc.client.IDfSessionManager;
+import com.documentum.fc.common.DfException;
+import com.google.enterprise.connector.dctm.dfcwrap.ICollection;
+import com.google.enterprise.connector.dctm.dfcwrap.IQuery;
+import com.google.enterprise.connector.dctm.dfcwrap.ISessionManager;
+import com.google.enterprise.connector.spi.RepositoryException;
+
+public class DmQuery implements IQuery {
+
+	IDfQuery idfQuery;
+
+	private static Logger logger = null;
+
+	static {
+		logger = Logger.getLogger(DmQuery.class.getName());
+	}
+
+	public DmQuery(IDfQuery idfQuery) {
+		this.idfQuery = idfQuery;
+	}
+
+	public DmQuery() {
+		this.idfQuery = new DfQuery();
+	}
+
+	public void setDQL(String dqlStatement) {
+
+		idfQuery.setDQL(dqlStatement);
+	}
+
+	public ICollection execute(ISessionManager sessionManager, int queryType)
+			throws RepositoryException {
+		if (!(sessionManager instanceof DmSessionManager)) {
+			throw new IllegalArgumentException();
+		}
+
+		DmSessionManager dmSessionManager = (DmSessionManager) sessionManager;
+		IDfSessionManager idfSessionmanager = dmSessionManager
+				.getDfSessionManager();
+
+		logger.info("value of IdfQuery " + idfQuery.getDQL());
+
+		IDfSession idfSession = null;
+		IDfCollection dfCollection = null;
+		try {
+			idfSession = idfSessionmanager.getSession(sessionManager
+					.getDocbaseName());
+			dfCollection = idfQuery.execute(idfSession, queryType);
+		} catch (DfException de) {
+			throw new RepositoryException(de);
+		}
+		
+		return new DmCollection(dfCollection);
+
+	}
+
+}
