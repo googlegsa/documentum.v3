918374f30e967b3d923d1f98e04b04b9ba5a367b
Sylvain Cauchy <scauchyster@gmail.com>
2008-10-07 14:42:33 +0000

    Documentum Connector changes linked to Issue 32.
    Analysis:
    
    --------
    When the timestamp field of the dm_audittrail table has the format
    mm/dd/yyyy hh:mm:ss, in the query which retrieves documents to delete the
    timestamp is formatted as yyyy-dd-mm ... instead of yyyy-mm-dd.
    
    This problem could cause an infinite loop or make the connector never
    return any document to delete.
    
    
    Change Log:
    ----------
    *  projects/google-enterprise-connector-dctm/source/java/com/google/enterprise/connector/dctm/DctmDocumentList.java
      - nextDocument() now calls the getTime() method on the Collection of documents to delete to get the time_stamp value (instead of getString()) 
      and creates a dctmSysobjectDocumentToDel with a ITime lastDeleteDate attribute.
     
    
    *  projects/google-enterprise-connector-dctm/source/java/com/google/enterprise/connector/dctm/DctmSysobjectDocument.java
      - The DctmSysobjectDocument constructor for deleted documents is changed to accept a ITime timestamp parameter instead of a String one.
      - the timeStamp String attribute is changed into a ITime attribute
      - findProperty() now calls asString() on the timeStamp attribute now formatted in the "mm/dd/yyyy hh:mi:ss" format by the getTime_pattern44() method. 
      The String such obtained is then parsed according to the "mm/dd/yyyy hh:mi:ss" pattern to create a Date object.
      
    
    *  projects/google-enterprise-connector-dctm/source/java/com/google/enterprise/connector/dctm/dfcwrap/ITime.java
      - Added asString() method returns a String
      - Added getTime_pattern44() getter returns a String
      
    
    *  projects/google-enterprise-connector-dctm/source/java/com/google/enterprise/connector/dctm/dfcwrap/ICollection.java
       - Added getTime() method returns an ITime object

---
+++ tags/release_1.1.0_r446/google-enterprise-connector-dctm/source/javatests/com/google/enterprise/connector/dctm/DctmMockAuthorizationManagerTest.java
@@ -0,0 +1,105 @@
+package com.google.enterprise.connector.dctm;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import com.google.enterprise.connector.spi.AuthorizationManager;
+import com.google.enterprise.connector.spi.AuthorizationResponse;
+import com.google.enterprise.connector.spi.Connector;
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.Session;
+
+import com.google.enterprise.connector.dctm.dctmmockwrap.DmInitialize;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class DctmMockAuthorizationManagerTest extends TestCase {
+
+	public final void testAuthorizeDocids() throws RepositoryException {
+
+		AuthorizationManager authorizationManager;
+		authorizationManager = null;
+		Connector connector = new DctmConnector();
+
+		((DctmConnector) connector).setLogin(DmInitialize.DM_LOGIN_OK1);
+		((DctmConnector) connector).setPassword(DmInitialize.DM_PWD_OK1);
+		((DctmConnector) connector).setDocbase(DmInitialize.DM_DOCBASE);
+		((DctmConnector) connector).setClientX(DmInitialize.DM_CLIENTX);
+		((DctmConnector) connector)
+				.setWebtop_display_url(DmInitialize.DM_WEBTOP_SERVER_URL);
+		((DctmConnector) connector).setIs_public("false");
+		Session sess = (DctmSession) connector.login();
+		authorizationManager = (DctmAuthorizationManager) sess
+				.getAuthorizationManager();
+		assertNotNull(authorizationManager);
+
+		{
+			String username = DmInitialize.DM_LOGIN_OK2;
+
+			Map expectedResults = new HashMap();
+			expectedResults.put(DmInitialize.DM_ID1, Boolean.TRUE);
+			expectedResults.put(DmInitialize.DM_ID2, Boolean.TRUE);
+			expectedResults.put(DmInitialize.DM_ID3, Boolean.TRUE);
+			expectedResults.put(DmInitialize.DM_ID4, Boolean.TRUE);
+			expectedResults.put(DmInitialize.DM_ID5, Boolean.FALSE);
+			assertNotNull((DctmAuthorizationManager) authorizationManager);
+			assertNotNull(expectedResults);
+			assertNotNull(username);
+			testAuthorization((DctmAuthorizationManager) authorizationManager,
+					expectedResults, username);
+		}
+
+		{
+			String username = DmInitialize.DM_LOGIN_OK3;
+
+			Map expectedResults = new HashMap();
+			expectedResults.put(DmInitialize.DM_ID1, Boolean.TRUE);
+			expectedResults.put(DmInitialize.DM_ID2, Boolean.FALSE);
+			expectedResults.put(DmInitialize.DM_ID3, Boolean.FALSE);
+			expectedResults.put(DmInitialize.DM_ID4, Boolean.FALSE);
+			expectedResults.put(DmInitialize.DM_ID5, Boolean.FALSE);
+			testAuthorization((DctmAuthorizationManager) authorizationManager,
+					expectedResults, username);
+		}
+
+		{
+			String username = DmInitialize.DM_LOGIN_OK1;
+
+			Map expectedResults = new HashMap();
+			expectedResults.put(DmInitialize.DM_ID1, Boolean.TRUE);
+			expectedResults.put(DmInitialize.DM_ID2, Boolean.TRUE);
+			expectedResults.put(DmInitialize.DM_ID3, Boolean.TRUE);
+			expectedResults.put(DmInitialize.DM_ID4, Boolean.TRUE);
+			expectedResults.put(DmInitialize.DM_ID5, Boolean.TRUE);
+
+			testAuthorization((DctmAuthorizationManager) authorizationManager,
+					expectedResults, username);
+		}
+
+	}
+
+	private void testAuthorization(
+			DctmAuthorizationManager authorizationManager, Map expectedResults,
+			String username) throws RepositoryException {
+
+		List docids = new LinkedList(expectedResults.keySet());
+
+		assertNotNull(docids);
+		List list = (List) authorizationManager.authorizeDocids(docids,
+				new DctmAuthenticationIdentity(username, null));
+		assertNotNull(list);
+		for (Iterator i = list.iterator(); i.hasNext();) {
+			AuthorizationResponse pm = (AuthorizationResponse) i.next();
+			String uuid = pm.getDocid();
+			boolean ok = pm.isValid();
+			Boolean expected = (Boolean) expectedResults.get(uuid);
+			Assert.assertEquals(username + " access to " + uuid, expected
+					.booleanValue(), ok);
+		}
+	}
+
+}
