7ef2e5752bb142aa0657a95e4f41ca7b1bbc2cdd
John Lacey <jlacey@google.com>
2009-04-11 07:34:56 +0000

    Fixed bug 45. Copied the UrlValidator class from the Livelink connector, which isn't as picky about the response, as long as it appears to be a valid URL. Removed HttpClient JAR files, since URL validation was the only use of it and the new code just uses java.net.URLConnection.

--- projects/google-enterprise-connector-dctm/source/java/com/google/enterprise/connector/dctm/DctmTraversalManager.java
+++ projects/google-enterprise-connector-dctm/source/java/com/google/enterprise/connector/dctm/DctmTraversalManager.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.logging.Logger;
 
-import org.apache.commons.httpclient.methods.DeleteMethod;
 import org.json.JSONException;
 import org.json.JSONObject;
 
