03f72ab2fd9af32d2557f1cd016cf0b19b0b4fb3
jl1615 <jl1615@a5724643-9123-0410-9cf5-31ddb559c8c8>
2010-12-02 05:20:02 +0000

    Merged trunk changes c628, r635:648, r653:655, and c658 onto the 2.6.x branch.

--- branches/2.6.x/projects/google-enterprise-connector-dctm/source/javatests/com/google/enterprise/connector/dctm/DctmMockConnectorTypeTest.java
+++ branches/2.6.x/projects/google-enterprise-connector-dctm/source/javatests/com/google/enterprise/connector/dctm/DctmMockConnectorTypeTest.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2009 Google Inc.
+// Copyright 2009 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,22 +16,19 @@ package com.google.enterprise.connector.dctm;
 
 import com.google.enterprise.connector.dctm.dctmmockwrap.DmInitialize;
 import com.google.enterprise.connector.dctm.dctmmockwrap.MockDmClient;
-import com.google.enterprise.connector.dctm.dctmmockwrap.MockDmDocbaseMap;
 import com.google.enterprise.connector.dctm.dfcwrap.IClient;
 import com.google.enterprise.connector.dctm.dfcwrap.IDocbaseMap;
-import com.google.enterprise.connector.dctm.dfcwrap.ISession;
 import com.google.enterprise.connector.spi.ConfigureResponse;
-import com.google.enterprise.connector.spi.ConnectorType;
 import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.util.UrlValidator;
 
 import junit.framework.TestCase;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DctmMockConnectorTypeTest extends TestCase {
   private static final String CLIENT_X_CLASS_NAME =
@@ -158,7 +155,7 @@ public class DctmMockConnectorTypeTest extends TestCase {
 
   public void testGcfClassNotFound() {
     type.setClientX("invalid.Class");
-    
+
     ConfigureResponse response = type.getConfigForm(Locale.US);
     assertIsFormError(response, "invalid.Class", "");
   }
@@ -199,7 +196,7 @@ public class DctmMockConnectorTypeTest extends TestCase {
 
   private void testGcfThrows(MockDmClientThrows client) {
     type.setClientX(client.getClass().getName());
-    
+
     ConfigureResponse response = type.getConfigForm(Locale.US);
     assertIsFormError(response, client.getMessage(), "");
   }
@@ -256,7 +253,7 @@ public class DctmMockConnectorTypeTest extends TestCase {
 
   public void testVcClassNotFound() {
     type.setClientX("invalid.Class");
-    
+
     ConfigureResponse response =
         type.validateConfig(validMap, Locale.US, null);
     assertIsError(response, "invalid.Class", "");
@@ -270,7 +267,7 @@ public class DctmMockConnectorTypeTest extends TestCase {
   private void testVcThrows(MockDmClientThrows client,
       Map<String, String> configMap, String message) {
     type.setClientX(client.getClass().getName());
-    
+
     ConfigureResponse response =
         type.validateConfig(configMap, Locale.US, null);
     assertIsError(response, message, "");
@@ -301,7 +298,7 @@ public class DctmMockConnectorTypeTest extends TestCase {
 
   public void testGpcfClassNotFound() {
     type.setClientX("invalid.Class");
-    
+
     ConfigureResponse response =
         type.getPopulatedConfigForm(validMap, Locale.US);
     assertIsFormError(response, "invalid.Class", "");
@@ -310,7 +307,7 @@ public class DctmMockConnectorTypeTest extends TestCase {
   private void testGpcfThrows(MockDmClientThrows client,
       Map<String, String> configMap) {
     type.setClientX(client.getClass().getName());
-    
+
     ConfigureResponse response =
         type.getPopulatedConfigForm(configMap, Locale.US);
     assertIsFormError(response, client.getMessage(), "");
@@ -327,124 +324,8 @@ public class DctmMockConnectorTypeTest extends TestCase {
   public void testGpcfGetSessionThrows() {
     ConfigureResponse response =
         type.getPopulatedConfigForm(emptyMap, Locale.US);
-    assertIsFormError(response, resources.getString("DEFAULT_ERROR_MESSAGE"), 
+    assertIsFormError(response, resources.getString("DEFAULT_ERROR_MESSAGE"),
         type.getConfigForm(Locale.US).getFormSnippet());
-  }
-
-  static class MultipleDocbases extends MockDmClient {
-    final int count;
-
-    MultipleDocbases(int count) {
-      this.count = count;
-    }
-
-    @Override
-    public IDocbaseMap getDocbaseMap() throws RepositoryException {
-      return new MockDmDocbaseMap(count);
-    }
-  }
-
-  /**
-   * Tests the {@code appendDropDownListAttribute} method.
-   *
-   * @param count the number of docbases to return
-   * @param value the current docbase value
-   * @param options the expected number of options in the select list
-   * @param selected the expected value of the selected option
-   */
-  private void testAddla(int count, String value, int options,
-      String selected) {
-    StringBuilder buf = new StringBuilder();
-    MockDmClient clientX = new MultipleDocbases(count);
-    try {
-      type.appendDropDownListAttribute(buf, value, resources, clientX);
-    } catch (RepositoryException e) {
-      fail(e.toString());
-    }
-    String selectList = buf.toString();
-    assertEquals(options, options(selectList));
-    assertEquals(selected, selected(selectList));
-  }
-
-  /**
-   * Tests the {@code appendDropDownListAttribute} method, and expects
-   * the current docbase value to be selected
-   *
-   * @param count the number of docbases to return
-   * @param value the current docbase value
-   * @param options the expected number of options in the select list
-   */
-  private void testAddla(int count, String value, int options) {
-    testAddla(count, value, options, value);
-  }
-
-  private int options(String selectList) {
-    return occurrences("<option ", selectList);
-  }
-
-  private String selected(String selectList) {
-    Matcher m = Pattern.compile("value=\"(.*)\" selected='selected'")
-        .matcher(selectList);
-    if (m.find()) {
-      assertEquals(1, m.groupCount());
-      return m.group(1);
-    } else {
-      return null;
-    }
-  }
-
-  private int occurrences(String pattern, String target) {
-    Matcher m = Pattern.compile(pattern).matcher(target);
-    int count = 0;
-    while (m.find()) {
-      count++;
-    }
-    return count;
-  }
-
-  /**
-   * When there are zero docbases, the empty string is added to the
-   * list and selected.
-   */
-  public void testGcfAddlaZero() {
-    testAddla(0, "", 1);
-  }
-
-  /** When there is only one docbase, it is the only option, and selected. */
-  public void testGcfAddlaOne() {
-    testAddla(1, "", 1, DmInitialize.DM_DOCBASE);
-  }
-
-  /**
-   * When there are multple docbases, the empty string is added to the
-   * list and selected.
-   */
-  public void testGcfAddlaMultiple() {
-    testAddla(2, "", 3);
-  }
-
-  public void testGpcfAddlaOne() {
-    testAddla(1, DmInitialize.DM_DOCBASE, 1);
-  }
-
-  public void testGpcfAddlaMultiple() {
-    testAddla(2, DmInitialize.DM_DOCBASE, 2);
-  }
-
-  /**
-   * When the current docbase is not in the list, it is added to the
-   * list and selected.
-   */
-  public void testGpcfAddlaInvalidZero() {
-    testAddla(0, "other", 1);
-  }
-
-  public void testGpcfAddlaInvalidOne() {
-    testAddla(1, "other", 2);
-  }
-
-  public void testGpcfAddlaInvalidMultiple() {
-    testAddla(2, "other", 3);
   }
 
   /**
@@ -473,7 +354,7 @@ public class DctmMockConnectorTypeTest extends TestCase {
 
     ConfigureResponse response =
         type.getPopulatedConfigForm(validMap, Locale.US);
-    assertIsFormError(response, resources.getString("DEFAULT_ERROR_MESSAGE"), 
+    assertIsFormError(response, resources.getString("DEFAULT_ERROR_MESSAGE"),
         modifiedForm);
   }
 }
