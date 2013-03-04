b6e371cb653c0751128fae24013606c20bed522c
John Lacey <jlacey@google.com>
2013-03-04 04:35:20 +0000

    Tagged the 3.1.0-M1 build /branches/dev/3.1.x@703.

---
+++ 3.1.x/3.1.0-M1/projects/dctm-core/source/javatests/com/google/enterprise/connector/dctm/FormSnippetTest.java
@@ -0,0 +1,191 @@
+// Copyright 2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.dctm;
+
+import com.google.enterprise.connector.dctm.dctmmockwrap.DmInitialize;
+import com.google.enterprise.connector.dctm.dctmmockwrap.MockDmDocbaseMap;
+import com.google.enterprise.connector.dctm.dfcwrap.IDocbaseMap;
+import com.google.enterprise.connector.spi.RepositoryException;
+
+import junit.framework.TestCase;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FormSnippetTest extends TestCase {
+  /** The Locale-specific resources. */
+  private ResourceBundle resources;
+
+  @Override
+  protected void setUp() {
+    // TODO: This is a hack to share DctmConnectorType.getResources,
+    // which is a trivial method.
+    DctmConnectorType type = new DctmConnectorType();
+    resources = type.getResources(Locale.US);
+  }
+
+  public void testGetJavaScript() {
+    // docbases = null, isAdvancedOn = true
+    FormSnippet snippet = new FormSnippet(resources, null, true);
+    InputStream in = snippet.getJavaScript();
+    assertNotNull(in);
+  }
+
+  /**
+   * Tests the {@code appendJavaScript} method when the JavaScript
+   * input stream is null.
+   */
+  public void testAppendJavaScript_notfound() {
+    // docbases = null, isAdvancedOn = true
+    FormSnippet snippet = new FormSnippet(resources, null, true) {
+        protected InputStream getJavaScript() { return null; } };
+    StringBuilder buf = new StringBuilder();
+
+    assertTrue(snippet.isAdvancedOn());
+    snippet.appendJavaScript(buf);
+    assertFalse(snippet.isAdvancedOn());
+    String js = buf.toString();
+    assertTrue(js, js.endsWith(resources.getString("javascript_error")));
+  }
+
+  public void testAppendJavaScript_noerror() {
+    // docbases = null, isAdvancedOn = true
+    FormSnippet snippet = new FormSnippet(resources, null, true);
+    StringBuilder buf = new StringBuilder();
+
+    assertTrue(snippet.isAdvancedOn());
+    snippet.appendJavaScript(buf);
+    assertTrue(snippet.isAdvancedOn());
+    String js = buf.toString();
+    assertTrue(js, js.indexOf(resources.getString("javascript_error")) == -1);
+    assertTrue(js, js.length() > 200); // There's *something* non-trivial. */
+  }
+
+  /**
+   * Tests the {@code appendDropDownListAttribute} method.
+   *
+   * @param count the number of docbases to return
+   * @param value the current docbase value
+   * @param options the expected number of options in the select list
+   * @param selected the expected value of the selected option
+   */
+  private void testAddla(int count, String value, int options,
+      String selected) {
+    StringBuilder buf = new StringBuilder();
+    // TODO: With the extraction of getDocbases, we could dispense
+    // with the multiple docbase feature of MockDmDocbaseMap.
+    try {
+      IDocbaseMap docbaseMap = new MockDmDocbaseMap(count);
+      List<String> docbases = new ArrayList<String>();
+      for (int i = 0; i < docbaseMap.getDocbaseCount(); i++) {
+        docbases.add(docbaseMap.getDocbaseName(i));
+      }
+      FormSnippet snippet = new FormSnippet(resources, docbases, true);
+
+      snippet.appendDropDownListAttribute(buf, value);
+    } catch (RepositoryException e) {
+      fail(e.toString());
+    }
+    String selectList = buf.toString();
+    assertEquals(options, options(selectList));
+    assertEquals(selected, selected(selectList));
+  }
+
+  /**
+   * Tests the {@code appendDropDownListAttribute} method, and expects
+   * the current docbase value to be selected
+   *
+   * @param count the number of docbases to return
+   * @param value the current docbase value
+   * @param options the expected number of options in the select list
+   */
+  private void testAddla(int count, String value, int options) {
+    testAddla(count, value, options, value);
+  }
+
+  private int options(String selectList) {
+    return occurrences("<option ", selectList);
+  }
+
+  private String selected(String selectList) {
+    Matcher m = Pattern.compile("value=\"(.*)\" selected='selected'")
+        .matcher(selectList);
+    if (m.find()) {
+      assertEquals(1, m.groupCount());
+      return m.group(1);
+    } else {
+      return null;
+    }
+  }
+
+  private int occurrences(String pattern, String target) {
+    Matcher m = Pattern.compile(pattern).matcher(target);
+    int count = 0;
+    while (m.find()) {
+      count++;
+    }
+    return count;
+  }
+
+  /**
+   * When there are zero docbases, the empty string is added to the
+   * list and selected.
+   */
+  public void testGcfAddlaZero() {
+    testAddla(0, "", 1);
+  }
+
+  /** When there is only one docbase, it is the only option, and selected. */
+  public void testGcfAddlaOne() {
+    testAddla(1, "", 1, DmInitialize.DM_DOCBASE);
+  }
+
+  /**
+   * When there are multple docbases, the empty string is added to the
+   * list and selected.
+   */
+  public void testGcfAddlaMultiple() {
+    testAddla(2, "", 3);
+  }
+
+  public void testGpcfAddlaOne() {
+    testAddla(1, DmInitialize.DM_DOCBASE, 1);
+  }
+
+  public void testGpcfAddlaMultiple() {
+    testAddla(2, DmInitialize.DM_DOCBASE, 2);
+  }
+
+  /**
+   * When the current docbase is not in the list, it is added to the
+   * list and selected.
+   */
+  public void testGpcfAddlaInvalidZero() {
+    testAddla(0, "other", 1);
+  }
+
+  public void testGpcfAddlaInvalidOne() {
+    testAddla(1, "other", 2);
+  }
+
+  public void testGpcfAddlaInvalidMultiple() {
+    testAddla(2, "other", 3);
+  }
+}
---
+++ 3.1.x/3.1.0-M1/projects/dctm-core/source/javatests/com/google/enterprise/connector/dctm/dctmmockwrap/MockDmSession.java
@@ -0,0 +1,109 @@
+// Copyright 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.dctm.dctmmockwrap;
+
+import com.google.enterprise.connector.dctm.dfcwrap.IId;
+import com.google.enterprise.connector.dctm.dfcwrap.IPersistentObject;
+import com.google.enterprise.connector.dctm.dfcwrap.ISession;
+import com.google.enterprise.connector.dctm.dfcwrap.ISessionManager;
+import com.google.enterprise.connector.dctm.dfcwrap.ISysObject;
+import com.google.enterprise.connector.dctm.dfcwrap.IType;
+import com.google.enterprise.connector.mock.MockRepositoryDocument;
+import com.google.enterprise.connector.mock.MockRepositoryDocumentStore;
+import com.google.enterprise.connector.mock.jcr.MockJcrRepository;
+import com.google.enterprise.connector.mock.jcr.MockJcrSession;
+import com.google.enterprise.connector.spi.RepositoryDocumentException;
+import com.google.enterprise.connector.spi.RepositoryException;
+
+public class MockDmSession implements ISession {
+  private final ISessionManager sessMgr;
+
+  private final MockJcrRepository mockRep;
+
+  private final String sessionFileNameSuffix;
+
+  public MockDmSession(ISessionManager sessMgr, MockJcrRepository mjR,
+      MockJcrSession mjS, String dbFileName) {
+    this.sessMgr = sessMgr;
+    this.mockRep = mjR;
+    this.sessionFileNameSuffix = dbFileName;
+  }
+
+  /**
+   * Constructs a session that cannot do much. It must, however,
+   * support {@link #getType}.
+   */
+  public MockDmSession() {
+    this(null, null, null, null);
+  }
+
+  MockRepositoryDocumentStore getStore() {
+    return mockRep.getRepo().getStore();
+  }
+
+  public String getLoginTicketForUser(String username) {
+    // this assumes that Mock authenticated the session by
+    // checking username==paswword
+    // /return mockJcrSession.getUserID();// The only security here is
+    return username;
+    // inherent to the fact that if
+    // authentication failed,
+    // Session==null the returning
+    // getUserId instead of directly
+    // retuning username would throw a
+    // nullPointerException
+  }
+
+  public String getLoginTicketEx(String username, String scope, int timeout,
+      boolean singleUse, String serverName) {
+    // See getLoginTicketForUser.
+    return username;
+  }
+
+  public String getDocbaseName() {
+    return this.sessionFileNameSuffix;
+  }
+
+  public String getServerVersion() {
+    return "dctmmockwrap";
+  }
+
+  public ISysObject getObject(IId objectId) throws RepositoryDocumentException {
+    MockRepositoryDocument mockRepositoryDocument = mockRep.getRepo()
+        .getStore().getDocByID(objectId.toString());
+    MockDmObject dctmMockRepositoryDocument = new MockDmObject(
+        mockRepositoryDocument);
+    return dctmMockRepositoryDocument;
+  }
+
+  public IType getType(String typeName) throws RepositoryException {
+    return new MockDmType(typeName);
+  }
+
+  public ISessionManager getSessionManager() throws RepositoryException {
+    return sessMgr;
+  }
+
+  public boolean isConnected() {
+    return sessMgr != null;
+  }
+
+  @Override
+  public IPersistentObject getObjectByQualification(String string)
+      throws RepositoryDocumentException {
+    // TODO Srinivas Auto-generated method stub
+    return null;
+  }
+}
